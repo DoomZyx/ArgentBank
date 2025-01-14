ee95ce4f9424a0ff29b52efe265a34ab3e3e9320
DoomZyx <axelcella.ac@gmail.com>
2025-01-14 15:18:31 +0100

    Réinstallation Backend + connexion utilisateur

--- Frontend/src/pages/Login/login.jsx
+++ Frontend/src/pages/Login/login.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import Footer from "../../components/Footer/footer";
 import Nav from "../../components/Header/nav";
 import "../../main.scss";
-
+import { loginUser } from "../../API/API";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await loginUser(email, password);
+
+      if (response && response.body && response.body.token) {
+        localStorage.setItem("token", response.body.token); // Stocker le token
+        setTimeout(() => {
+          window.location.href = "/user-page";
+        }, 500);
+      } else {
+        const errorData = await response.json();
+        setError(errorData.message || "Une erreur est survenue.");
+      }
+    } catch (error) {
+      setError("Erreur de connexion. Veuillez réessayer.");
+    }
+  };
 
   return (
     <>
@@ -12,12 +37,14 @@ function Login() {
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
           <h1>Sign In</h1>
-          <form id="form">
+          <form id="form" onSubmit={handleSubmit}>
             <div className="input-wrapper">
               <label htmlFor="username">Username</label>
               <input
                 type="text"
                 id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="input-wrapper">
@@ -25,6 +52,8 @@ function Login() {
               <input
                 type="password"
                 id="password"
+                value={password}
+                onChange={(e) => setpassword(e.target.value)}
               />
             </div>
             <div className="input-remember">
@@ -34,6 +63,7 @@ function Login() {
             <button type="submit" className="sign-in-button">
               Sign In
             </button>
+            {error && <p className="error">{error}</p>}
           </form>
         </section>
       </main>
